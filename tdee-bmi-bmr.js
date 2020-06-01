6ff4ec99a87a422b6a39d24bb794e08160180562
Marco <dead_legend@hotmail.com>
2020-06-01 22:13:20 +0200

    Create function for updating weight and height units according to user selection (imperial/metric).

---
+++ tdee-bmi-bmr.js
@@ -0,0 +1,26 @@
+function updateToImperial() {
+
+    const weight = document.getElementById('weight');
+
+    const height = document.getElementById('height');
+
+    weight.value = 'lbs';
+        
+    height.value = 'ft\' in"';
+
+}
+
+function updateToMetric() {
+
+    const weight = document.getElementById('weight');
+
+    const height = document.getElementById('height');
+
+    weight.value = 'kg';
+
+    height.value = 'cm';
+
+}
+
+
+
