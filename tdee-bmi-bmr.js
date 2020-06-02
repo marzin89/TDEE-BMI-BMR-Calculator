b45f3c081d4ffc51ddfd35b214f234c00f7997b7
Marco <dead_legend@hotmail.com>
2020-06-02 18:27:22 +0200

    Create function to calculate BMI.

--- tdee-bmi-bmr.js
+++ tdee-bmi-bmr.js
@@ -22,5 +22,33 @@ function updateToMetric() {
 
 }
 
+function calculateBMI() {
+
+    const weight = document.getElementById('weight');
+
+    const height = document.getElementById('height');
+
+    const imperial = document.getElementById('Imperial');
+
+    const metric = document.getElementById('Metric');
+
+    let result;
+
+    if ( imperial.checked == true ) {
+
+        result = 'Your BMI is ' + Math.round( 703 * weight.value / ( height.value * height.value )) + '.';
+
+        alert( result );
+
+    } else if ( metric.checked == true ) {
+
+        result = 'Your BMI is ' + Math.round( weight.value / ( height.value * height.value )) + '.';
+
+        alert( result );
+
+    }
+
+}
+
 
 
